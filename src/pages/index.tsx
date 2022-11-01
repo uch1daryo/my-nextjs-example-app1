e21e3f0bb23ea3a6f65498644147ca0c3a55e723
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-11-01 06:47:14 +0000

    feat: added Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
 export default function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
