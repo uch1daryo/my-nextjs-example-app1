b0f5b071ddfed9235f5c12285ca5f218beb649ab
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-11-01 23:29:07 +0000

    feat: updated Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,18 @@
+import Container from "@mui/material/Container";
+import Hero from "./Hero";
+import Overview from "./Overview";
+import Message from "./Message";
+import News from "./News";
+import Social from "./Social";
+
 export default function Main() {
-  return <></>;
+  return (
+    <Container maxWidth="lg">
+      <Hero />
+      <Overview />
+      <Message />
+      <News />
+      <Social />
+    </Container>
+  );
 }
