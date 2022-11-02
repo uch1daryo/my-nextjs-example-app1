ff1a8efcd42afac24d20f4cbcc1b9168dacf1bc5
Uchida Ryo <uchidaryo.gm@gmail.com>
2022-11-02 04:42:41 +0000

    feat: updated Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,50 @@
-export default function Hero() {
-  return <></>;
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+type HeroPostProps = {
+  post: {
+    title: string;
+    description: string;
+  };
+};
+
+export default function Hero(props: HeroPostProps) {
+  const { post } = props;
+  const heroImage = "/hero.jpg";
+
+  return (
+    <Paper
+      sx={{
+        position: "relative",
+        backgroundColor: "grey.700",
+        color: "#fff",
+        mb: 4,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundImage: `url(${heroImage})`,
+      }}
+    >
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: "relative",
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h4" color="inherit" gutterBottom>
+              {post.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {post.description}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
